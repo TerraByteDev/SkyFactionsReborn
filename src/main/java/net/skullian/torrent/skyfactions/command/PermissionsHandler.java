087c9900ffea5968dcb24dcbd35f3e89dafa0360
Skullian <133133667+Skullians@users.noreply.github.com>
2024-09-18 20:37:07 +0100

    debugs for perms. reduced jar size from 5mb to 1.82

--- src/main/java/net/skullian/torrent/skyfactions/command/PermissionsHandler.java
+++ src/main/java/net/skullian/torrent/skyfactions/command/PermissionsHandler.java
@@ -9,6 +9,8 @@ public class PermissionsHandler {
 
     public static boolean hasPerm(Player player, List<String> node, boolean sendDeny) {
         for (String perm : node) {
+            System.out.println(perm);
+            System.out.println(player.hasPermission(perm));
             if (player.hasPermission(perm)) return true;
         }
 
