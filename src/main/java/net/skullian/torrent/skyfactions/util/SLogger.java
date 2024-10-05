e2ecf677c5f790ed0e1f17d71cf199f320cf6fc6
Skullian <133133667+Skullians@users.noreply.github.com>
2024-10-06 00:19:35 +0200

    Update SLogger.java

--- src/main/java/net/skullian/torrent/skyfactions/util/SLogger.java
+++ src/main/java/net/skullian/torrent/skyfactions/util/SLogger.java
@@ -8,20 +8,20 @@ public class SLogger {
     public static final ComponentLogger LOGGER = ComponentLogger.logger("SkyFactionsReborn");
 
     public static void info(Object message, Object... args) {
-        synchronized (SkyFactionsReborn.class) {
+
             LOGGER.info("\u001B[34m" + message + "\u001B[0m", args);
-        }
+        
     }
 
     public static void warn(Object message, Object... args) {
-        synchronized (SkyFactionsReborn.class) {
+
             LOGGER.warn(message + "\u001B[0m", args);
-        }
+        
     }
 
     public static void fatal(Object message, Object... args) {
-        synchronized (SkyFactionsReborn.class) {
+
             LOGGER.error(message + "\u001B[0m", args);
-        }
+        
     }
 }
