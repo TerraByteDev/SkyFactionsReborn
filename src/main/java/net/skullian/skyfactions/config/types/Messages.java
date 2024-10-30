00fa82f5a1d387bea34641fd14f5ecba49597856
Skullian <133133667+Skullians@users.noreply.github.com>
2024-10-30 13:34:39 +0000

    fix GUIs

--- src/main/java/net/skullian/skyfactions/config/types/Messages.java
+++ src/main/java/net/skullian/skyfactions/config/types/Messages.java
@@ -281,7 +281,7 @@ public enum Messages {
     private static void registerGUIs(File dir, String locale) throws IOException {
         Map<String, YamlDocument> docs = new HashMap<>();
         for (GUIEnums enumEntry : GUIEnums.values()) {
-            YamlDocument doc = YamlDocument.create(new File(dir, enumEntry.getConfigPath()),
+            YamlDocument doc = YamlDocument.create(new File(dir, enumEntry.getConfigPath(), SkyFactionsReborn.getInstance().getResource("language/" + locale + "/guis/" + enumEntry.getConfigPath() + ".yml")),
                     GeneralSettings.DEFAULT, LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("CONFIG_VERSION")).build());
 
             docs.put(enumEntry.getConfigPath(), doc);
