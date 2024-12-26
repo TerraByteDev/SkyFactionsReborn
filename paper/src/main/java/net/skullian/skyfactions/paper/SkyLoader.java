6f4dc3c46541b56470355b2d2c46ac5622db1459
Skullian <133133667+Skullians@users.noreply.github.com>
2024-12-26 21:28:11 +0000

    Init: Add MultiLib to gradle & paper classloader

--- paper/src/main/java/net/skullian/skyfactions/paper/SkyLoader.java
+++ paper/src/main/java/net/skullian/skyfactions/paper/SkyLoader.java
@@ -47,6 +47,7 @@ public class SkyLoader implements PluginLoader {
         centralResolver.addDependency(new Dependency(new DefaultArtifact("org.jooq:jooq:3.19.16"), null));
         centralResolver.addDependency(new Dependency(new DefaultArtifact("org.flywaydb:flyway-core:11.1.0"), null));
         centralResolver.addDependency(new Dependency(new DefaultArtifact("com.google.guava:guava:33.3.1-jre"), null));
+        centralResolver.addDependency(new Dependency(new DefaultArtifact("com.github.puregero:multilib:1.2.4"), null));
 
 
         pluginClasspathBuilder.addLibrary(invUIResolver);
