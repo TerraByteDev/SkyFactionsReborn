970bd9aefda9624a82e68b96faa64487627263f6
Skullian <133133667+Skullians@users.noreply.github.com>
2025-01-02 18:04:46 +0000

    Fix: Codegen

--- common/src/main/java/net/skullian/skyfactions/common/util/MigrationUtil.java
+++ common/src/main/java/net/skullian/skyfactions/common/util/MigrationUtil.java
@@ -4,6 +4,7 @@ import net.skullian.skyfactions.common.api.SkyApi;
 import org.flywaydb.core.api.migration.Context;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
 
@@ -12,7 +13,10 @@ public class MigrationUtil {
     public static DSLContext getCtx(Context context) {
         Configuration configuration = new DefaultConfiguration()
                 .set(context.getConnection())
-                .set(SkyApi.getInstance().getDatabaseManager().getDialect());
+                .set(System.getProperty("net.skullian.codegen").equals("true")
+                        ? SQLDialect.SQLITE
+                        : SkyApi.getInstance().getDatabaseManager().getDialect()
+                );
 
         return DSL.using(configuration);
     }
